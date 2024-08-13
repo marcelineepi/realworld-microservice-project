d6e00b24efcc6cb88c281b21969dae4d25e3413f
marcelineepi <149755545+marcelineepi@users.noreply.github.com>
2024-08-13 11:12:20 -0400

    Update installations.sh

--- installtions/installations.sh
+++ installtions/installations.sh
@@ -24,6 +24,13 @@ sudo chmod 777 /var/run/docker.sock
 docker version
 sudo usermod -aG docker jenkins
 
+# install trivy
+sudo apt-get install wget apt-transport-https gnupg
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
 # Install Terraform
 sudo apt install wget -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
